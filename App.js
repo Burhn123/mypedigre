14fe7838d80991512926061c8f70c879ecb6553d
Burhn123 <brhn.zebek@gmail.com>
2024-11-10 21:58:46 +0300

    firs commit
    first commit denemesi

---
+++ App.js
@@ -0,0 +1,88 @@
+import { StatusBar } from 'expo-status-bar';
+import { 
+  StyleSheet, 
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  Button,
+   } from 'react-native';
+
+   import React, {useState} from 'react';
+   import Loading from './src/components/Loading';
+
+export default function App() {
+  const [name,setName] = useState("")
+  const [lastName,setLastname] = useState("")
+  const [result,setResult] =useState('');
+  const [isLoading,setIsLoading]=useState(false)
+
+  return (
+    <View style={styles.container}>
+      <Text>Hos Geldin {result}</Text>
+      <Text>Kullanıcı adı</Text>
+      <TextInput
+      inputMode='email'
+      style={styles.textInputStyle}
+      placeholder='Kullanıcı adı giriniz'
+      onChangeText={setName}
+      value={name}
+      />
+      <Text>Şifre</Text>
+      <TextInput
+      placeholder='şifrenizi giriniz'
+      style={styles.textInputStyle}
+      onChangeText={setLastname}
+      value={lastName}
+      secureTextEntry={true}
+      />
+      <Pressable 
+        onPress={()=>setIsLoading(true)}
+        style={({pressed}) => [{backgroundColor:pressed ?"gray": 'blue'},styles.button]}>
+        <Text style={styles.buttonText}> GİRİS</Text>
+      </Pressable>
+        {isLoading ? <Loading name="" changeIsLoading={()=>setIsLoading(false)}/> :null }
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  textInputStyle:{
+    borderWidth:1,
+    width:'80%',
+    height:50,
+    borderRadius:10,
+    marginVertical:10,
+    textAlign:'center'
+  },
+
+  button: {
+    borderWidth:1,
+    width:'80%',
+    height:50,
+    borderRadius:10,
+    alignItems:'center',
+    justifyContent:'center',
+    
+
+  },
+
+  buttonText:{
+    fontWeight:'bold',
+    color:'white',
+
+  }
+  
+
+  
+
+  
+});
