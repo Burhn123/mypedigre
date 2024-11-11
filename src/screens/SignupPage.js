bb2508b42a484063340197d550bd515c32956c04
Burhn123 <brhn.zebek@gmail.com>
2024-11-11 10:00:33 +0300

    11.11.2024

---
+++ src/screens/SignupPage.js
@@ -0,0 +1,17 @@
+import { StyleSheet,Text,View } from "react-native";
+import React from "react";
+
+const SignupPage =()=> {
+
+    return (
+
+        <View>
+            <Text>SignupPage</Text>
+            </View>
+
+
+    )
+}
+
+export default SignupPage
+const style =StyleSheet.create({})
