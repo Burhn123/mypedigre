513d91eb5fa0873022b40443b02ab34b3f62b48f
Burhn123 <brhn.zebek@gmail.com>
2024-11-11 12:03:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
-import React from 'react';
-
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 import LoginPage from './src/screens/LoginPage'
 import SignupPage from './src/screens/SignupPage'
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
+
     <NavigationContainer>
-      <LoginPage />
-      <SignupPage />
-      
+      <Stack.Navigator>
+
+        <Stack.Screen name='Login' component={LoginPage}/>
+
+        <Stack.Screen name='SigupPage'component={SignupPage}/>
+
+      </Stack.Navigator>
     </NavigationContainer>
+    
   )
 }
 
-export default App;
+export default App
+
+const styles = StyleSheet.create({})
+
+
+
+
+
+
+
+
+
+
+    
