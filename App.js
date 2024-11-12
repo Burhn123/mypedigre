330c196f5da886c2a090a7b0d771ea211b2d4b84
Burhn123 <brhn.zebek@gmail.com>
2024-11-12 09:00:12 +0300

    de

--- App.js
+++ App.js
@@ -1,30 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-<<<<<<< HEAD
 import LoginPage from './src/screens/LoginPage';
 import SignupPage from './src/screens/SignupPage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Tanitim  from './src/screens/Tanitim'; //DENEME TANITIM SAYFASI
 
-=======
-import RootNavigation from './src/navigation/RootNavigation';
->>>>>>> 9234c9ef55b84c9c6ba263e6fd5a316f7143bfef
 
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}} >
-        <Stack.Screen name='LoginPage' component={LoginPage} />
-        <Stack.Screen name='SignupPage' component={SignupPage} />
-        <Stack.Screen name='Tanitim' component={Tanitim} />      
-      </Stack.Navigator>
-    </NavigationContainer>
-=======
    <RootNavigation/>
->>>>>>> 9234c9ef55b84c9c6ba263e6fd5a316f7143bfef
   );
 };
 
