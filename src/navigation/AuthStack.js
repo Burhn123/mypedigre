c5ba4be18f895700b0dde8e27be223a13a9518ed
Burhn123 <brhn.zebek@gmail.com>
2024-11-12 18:49:06 +0300

    gizem
    18:48 commiti

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -16,12 +16,9 @@ const AuthStack = () =>{
         screenOptions={{headerShown:false}} >
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Signup" component={SignupPage} />
-<<<<<<< HEAD
             <Stack.Screen name="Tanitim" component={Tanitim} />
             <Stack.Screen name="Iletisim" component={Iletisim} />
             
-=======
->>>>>>> 91d0a377dc8966244cc54ec91661c46543bf2c97
         </Stack.Navigator>
 
     
@@ -31,3 +28,36 @@ const AuthStack = () =>{
 
 export default AuthStack;
 
+/*
+import React from "react";
+import { LoginPage , SignupPage , Tanitim , Iletisim  } from "../screens";
+import { createStackNavigator } from '@react-navigation/stack';
+import { Settings } from "react-native";
+
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+
+
+const AuthStack = () =>{
+    return(
+    
+        <Stack.Navigator 
+        initialRouteName="Login"
+        screenOptions={{headerShown:false}} >
+            <Stack.Screen name="Login" component={LoginPage} />
+            <Stack.Screen name="Signup" component={SignupPage} />
+        </Stack.Navigator>
+
+    
+
+    )
+}
+
+export default AuthStack;
+
+
+
+
+
+*/
