93a0e45f9926ebf6823ed55ca3477864c09281ef
Burhn123 <brhn.zebek@gmail.com>
2024-12-07 21:27:19 +0300

    deneme

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,37 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../../firebaseConfig";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC-EickVY9htkNatAMikYNV5wJ59o2KWMA",
-  authDomain: "mypedigre-b3a37.firebaseapp.com",
-  projectId: "mypedigre-b3a37",
-  storageBucket: "mypedigre-b3a37.firebasestorage.app",
-  messagingSenderId: "22484854534",
-  appId: "1:22484854534:web:83cc84c845278c8232e087",
-  measurementId: "G-F4M6ZB8CSH"
+const HomePage = () => {
+  const sendData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "reactNativeLesson"), {
+        title: "Zero to Hero",
+        content: "React Native tutorial for beginner",
+        lesson: 95,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>HomePage</Text>
+      {/* Buton Eklendi */}
+      <Button title="Send Data" onPress={sendData} />
+    </View>
+  );
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export default HomePage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
