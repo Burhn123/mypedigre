7f0b70cde7cb222a0ea3954e66ad964ef64ec7bc
Burhn123 <brhn.zebek@gmail.com>
2024-12-07 21:05:58 +0300

    deneme

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,10 +1,33 @@
-import { StyleSheet,Text,View } from "react-native";
+import { StyleSheet,Text,View,Button } from "react-native";
 import React from "react";
+import { collection, addDoc } from "firebase/firestore"; 
+import { db } from "../../firebaseConfig";
+
 
 const HomePage = () =>{
+    const sendData =async()=>{
+        try {
+            const docRef = await addDoc(collection(db, "reactNativeLesson"), {
+              title: "Zero to Hero",
+              content: "React Native tutorial for beginner",
+              lesson: 95
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+
+
+    }
+
+
+
+
     return(
         <View style={styles.container}>
             <Text>HomePage</Text>
+            <Button title="Veri girisi" handleOnPress={sendData} />
+            
         </View>
     )
 }
@@ -17,4 +40,8 @@ const styles = StyleSheet.create({
         justifyContent:"center",
         alignItems:"center"
     }
+
+   
+
+    
 })
